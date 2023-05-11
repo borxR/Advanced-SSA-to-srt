13b7afc6fbdb3ca8de30856a05481863dc822265
borxR <94912459+borxR@users.noreply.github.com>
2023-05-11 22:18:54 +0530

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,43 @@
+
+
+def convert_ssa_to_srt(ssa_file_path, srt_file_path):
+    with open(ssa_file_path, 'r', encoding='utf-8-sig') as f:
+        ssa_lines = f.readlines()
+
+    srt_lines = []
+    current_line_number = 1
+
+    for line in ssa_lines:
+        line = line.strip()
+        if line.startswith('Dialogue:'):
+            fields = line.split(',')
+            start_time = convert_ssa_time_to_seconds(fields[1])
+            end_time = convert_ssa_time_to_seconds(fields[2])
+            subtitle_text = ','.join(fields[9:]).replace('\\N', '\n')
+            subtitle_lines = subtitle_text.split('\n')
+            for subtitle_line in subtitle_lines:
+                srt_lines.append(str(current_line_number))
+                srt_lines.append("\n")
+                srt_lines.append(f"{convert_seconds_to_srt_time(start_time)} --> {convert_seconds_to_srt_time(end_time)}")
+                srt_lines.append("\n")
+                srt_lines.append(subtitle_line)
+                srt_lines.append("\n\n")
+                current_line_number += 1
+
+    with open(srt_file_path, 'w') as f:
+        f.writelines(srt_lines)
+
+def convert_ssa_time_to_seconds(ssa_time):
+    h, m, s = ssa_time.split(':')
+    s, ms = s.split('.')
+    return int(h)*3600 + int(m)*60 + int(s) + int(ms)/1000
+
+def convert_seconds_to_srt_time(seconds):
+    ms = int((seconds - int(seconds)) * 1000)
+    seconds = int(seconds)
+    m, s = divmod(seconds, 60)
+    h, m = divmod(m, 60)
+    return f"{h:02d}:{m:02d}:{s:02d},{ms:03d}"
+
+
+convert_ssa_to_srt('path to ass file','output path.srt')
